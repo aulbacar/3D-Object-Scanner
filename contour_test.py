56c0fa89de0c129167aa0516624aaadaff263e2e
aulbacar <carson_aulbaugh@outlook.com>
2023-04-24 17:27:39 -0500

    new data

--- contour_test.py
+++ contour_test.py
@@ -23,14 +23,15 @@ y_arr = []
 #z_arr = []
 ang_arr = []
 contour_array = []
-while(i < 1):
-    image = cv.imread('Sample_Data/Hole_Lit/test' + str(i) + '.jpg')
+while(i < 25):
+    image = cv.imread('Sample_Data/Laser_Bottle/test' + str(i) + '.jpg')
     #cv.waitKey(0)
     assert image is not None, "file could not be read, check with os.path.exists()"
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(gray, 127, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    #cv.imshow('Canny Edges After Contouring', thresh)
+    cv.imshow('Canny Edges After Contouring', thresh)
+    cv.imshow('Contours', image)
     #print(contours)
     toTuple(contours, x_arr, y_arr, i)
     #print(x_arr)
@@ -45,19 +46,19 @@ while(i < 1):
 
 #print(ang_arr[1160])
 #print(ang_arr)
-fig = plt.figure()
+#fig = plt.figure()
 #ax = fig.add_subplot(projection='3d')
 #ax.plot(x_arr, y_arr)
 #ROTATE AROUND Z-AXIS
 i = 0
-while(i < len(x_arr)):
+#while(i < len(x_arr)):
     #fig = plt.figure()
     #ax = fig.add_subplot(projection='3d')
-    plt.plot(x_arr[i], y_arr[i])
+    #plt.plot(x_arr[i], y_arr[i])
     #ax.scatter(x_arr[i], y_arr[i], ang_arr[i])
-    plt.draw()
-    plt.pause(.2)
-    i += 1
+    #plt.draw()
+    #plt.pause(.2)
+   # i += 1
 #for coord in x_arr:
     #ax.plot3D(x_arr[coord], ang_arr[coord], y_arr[coord])
     #plt.show()
@@ -65,9 +66,9 @@ while(i < len(x_arr)):
 #plt.scatter(x_arr, y_arr, ang_arr)
 #plt.show()
 #print(y_arr)
-print(len(x_arr))
-print(len(y_arr))
-print(len(ang_arr))
+#print(len(x_arr))
+#print(len(y_arr))
+#print(len(ang_arr))
 
 #plt.plot(x_arr, y_arr)
 #plt.show()
--- geeks_contour_test.py
+++ geeks_contour_test.py
@@ -3,32 +3,36 @@ import numpy as np
   
 # Let's load a simple image with 3 black squares
 
+i = 0
+while(i < 25):
+    image = cv2.imread('Sample_Data/Laser_Bottle/test' + str(i) + '.jpg')
 
-image = cv2.imread('Sample_Data/Keys_Lit/test23.jpg')
-cv2.waitKey(0)
+#image = cv2.imread('Sample_Data/Laser_Bottle/test23.jpg')
+    cv2.waitKey(0)
   
 # Grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   
 # Find Canny edges
-edged = cv2.Canny(gray, 30, 200)
-cv2.waitKey(0)
+    edged = cv2.Canny(gray, 30, 200)
+    cv2.waitKey(0)
   
 # Finding Contours
 # Use a copy of the image e.g. edged.copy()
 # since findContours alters the image
-contours, hierarchy = cv2.findContours(edged, 
-    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(edged, 
+        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
   
-cv2.imshow('Canny Edges After Contouring', edged)
-cv2.waitKey(0)
+    cv2.imshow('Canny Edges After Contouring', edged)
+    cv2.waitKey(0)
   
-print("Number of Contours found = " + str(len(contours)))
+    print("Number of Contours found = " + str(len(contours)))
   
 # Draw all contours
 # -1 signifies drawing all contours
-cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
 #print(contours)
-cv2.imshow('Contours', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('Contours', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    i += 1
