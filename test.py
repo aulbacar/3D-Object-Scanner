b6b938625ee1ab3bdec0e82f1cb24a98cbd2b677
aulbacar <carson_aulbaugh@outlook.com>
2023-04-28 02:05:41 -0500

    straightening width coordinates

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-img = cv.imread('Sample_Data/Laser_Bottle/test1.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('Sample_Data/Laser_Bottle/test20.jpg', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 #img = cv.medianBlur(img,5)
 ret,th1 = cv.threshold(img,200,255,cv.THRESH_BINARY)
@@ -22,6 +22,7 @@ arr = []
 temp = []
 maxy = 0
 maxx = 0
+
 while(i < len(th1)):
     while(c < len(th1[0])):
         if(th1[i][c] == 255):
@@ -40,23 +41,77 @@ i = 0
 while(i < len(arr)):
     #print(arr[i][0])
     arr[i][1] = maxx - arr[i][0]
-    arr[i][0] = 1
+    #arr[i][0] = 1
     i += 1
 i = 0
 
+arrx = []
+arry = []
+arrz = []
 filter_arr = []
 while(i < len(arr)):
     if(arr[i][1] > 0 and arr[i][1] < 150):
         filter_arr.append(arr[i])
+        arrx.append(arr[i][0])
+        arry.append(arr[i][1])
+        arrz.append(arr[i][2])
     
     i += 1
 
+#ax = plt.axes(projection = '3d')
+#ax.plot3D(filter_arr, 'gray')
 
-print(maxy)
-print(maxx)
+#print(maxy)
+#print(maxx)
 #print(arr)
-print(filter_arr)
+#print(filter_arr)
+unique_arrz = list(set(arrz))
+#print(unique_arrz)
 
+
+
+maxx_at_z = 0
+minx_at_z = 480
+i = 0
+c = 0
+minx_arr = []    
+while(i < len(unique_arrz)):
+    try_val = unique_arrz[i]
+    while(c < len(filter_arr)):
+        if(filter_arr[c][2] == try_val):
+            #print(filter_arr[c])
+            #if(filter_arr[c][0] > maxx_at_z):
+                #maxx_at_z = filter_arr[c][0]
+                #print("max ", maxx_at_z)
+            if(filter_arr[c][0] < minx_at_z):
+                minx_at_z = filter_arr[c][0]
+                #print("min", minx_at_z)
+                #print(filter_arr[c])
+                minx_arr.append(filter_arr[c][0])
+        c += 1
+    c = 0
+    minx_at_z = 480
+    i += 1
+#print(minx_arr)
+#ax = plt.axes(projection = '3d')
+##ax.plot3D(arrx, arry, arrz, 'gray')
+#plt.show()
+
+#print(minx_arr)
+i = 0
+c = 0
+#print(len(minx_arr))
+#print(len(unique_arrz))
+straight_filter_arr = []
+while(i < len(filter_arr)):
+    while(c < len(unique_arrz)):
+        if(filter_arr[i][2] == unique_arrz[c]):
+            filter_arr[i][0] = filter_arr[i][0] - int(minx_arr[c]) + 1
+            straight_filter_arr.append(filter_arr[i])
+        c += 1
+    c = 0
+    i += 1
+#print(straight_filter_arr)
 
 
 
